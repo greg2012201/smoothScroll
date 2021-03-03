db01c177f3af74947ef20ffbccace77418b81230
Grzegorz Dubiel <greg2012201@gmail.com>
2021-03-03 13:08:00 +0100

    Add the set scrollPosition function

--- src/tools/smoothScroll.js
+++ src/tools/smoothScroll.js
@@ -1,24 +1,22 @@
 const smoothScrollContainer = document.querySelector('.smooth-scroll-container');
-const body = document.querySelector('body');
+let scroll = window.scrollY || window.pageYOffset;
 const containerStyles = smoothScrollContainer.style;
 export const smoothScrollInit = () => {
 
 
 
-    document.addEventListener('scroll', moveOnScroll);
-
+    document.addEventListener('scroll', setScrollPosition);
+    requestAnimationFrame(moveOnScroll);
 
 
     setContainersProperties();
 }
+
 const setContainersProperties = () => {
 
+    setMainContainerStyle();
+    setParentContainerStyle();
 
-    containerStyles.position = 'fixed';
-    containerStyles.left = '0';
-    containerStyles.top = '0';
-    containerStyles.width = '100%';
-    containerStyles.overflow = 'hidden';
 
     setTimeout(() => {
         smoothScrollContainer.style.transition = 'transform 0.4s linear';
@@ -27,15 +25,28 @@ const setContainersProperties = () => {
 
 
 }
+const setMainContainerStyle = () => {
+
+    containerStyles.position = 'fixed';
+    containerStyles.left = '0';
+    containerStyles.top = '0';
+    containerStyles.width = '100%';
+    containerStyles.overflow = 'hidden';
+    containerStyles.transform = `translateY(${-scrollY}px)`;
+
+}
+
+const setParentContainerStyle = () => {
 
 
+    smoothScrollContainer.parentElement.style.height = containerStyles.height;
 
-
-
-function moveOnScroll() {
-    const scroll = window.scrollY || window.pageYOffset;
-
+}
+const setScrollPosition = () => {
+    scroll = window.scrollY || window.pageYOffset;
+}
+const moveOnScroll = () => {
 
     containerStyles.transform = `translateY(${-scroll}px)`;
-
+    requestAnimationFrame(moveOnScroll);
 }
