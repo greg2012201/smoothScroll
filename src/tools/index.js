6ee36a7b0c003593767221c925ec018f4352e7e6
Grzegorz Dubiel <greg2012201@gmail.com>
2021-03-05 14:31:42 +0100

    Change the sample timing function in the Index & add Error handling to smoothScroll

--- src/tools/index.js
+++ src/tools/index.js
@@ -1,22 +1,11 @@
 import {
-    setScrollAnimationProperties,
-    smoothScrollContainerInit,
+
+    smoothScrollInit,
 
 } from './smoothScroll';
 const time = '1000ms';
 const timingFunction = 'cubic-bezier(0.23, 1, 0.32, 1)';
 
 addEventListener('DOMContentLoaded', () => {
-    smoothScrollContainerInit();
-    setScrollAnimationProperties(time, timingFunction);
-});
-
-addEventListener('wheel', () => {
-    console.log('wywołanie wheel');
-    setScrollAnimationProperties(time, timingFunction)
-});
-addEventListener('touchmove', () => {
-
-    console.log('wywołanie touch');
-    setScrollAnimationProperties(time, timingFunction)
+    smoothScrollInit(time, timingFunction);
 });
--- src/tools/smoothScroll.js
+++ src/tools/smoothScroll.js
@@ -1,8 +1,45 @@
 const smoothScrollContainer = document.querySelector('.smooth-scroll-container');
 const containerStyles = smoothScrollContainer.style;
 let scroll = window.scrollY || window.pageYOffset;
+let isValid = false;
 
-export const smoothScrollContainerInit = () => {
+
+export const smoothScrollInit = (time, timingFunction) => {
+    paramsValid(time, timingFunction);
+
+    if (isValid) {
+        smoothScrollContainerInit(time, timingFunction);
+        initListiners(time, timingFunction);
+
+    }
+}
+export const smoothScrollTo = (target, time, timingFunction) => {
+    paramsValid(time, timingFunction);
+    containerStyles.transform = `translateY(${-target}px)`;
+    scrollTo(0, target);
+    setScrollAnimationProperties(time, timingFunction);
+}
+const paramsValid = (time, timingFunction) => {
+
+    const valid = typeof (time) === 'string' && typeof (timingFunction) === 'string';
+    if (valid) {
+        isValid = true;
+    } else {
+        throw Error('The params value must be a type of string. Valid value must be similar to: "1000ms","linear"');
+    }
+
+}
+const initListiners = (time, timingFunction) => {
+    addEventListener('wheel', () => {
+
+        setScrollAnimationProperties(time, timingFunction)
+    });
+    addEventListener('touchmove', () => {
+        setScrollAnimationProperties(time, timingFunction)
+    });
+}
+
+const smoothScrollContainerInit = () => {
     document.addEventListener('scroll', () => {
         setScrollPosition();
 
@@ -16,7 +53,7 @@ const setContainersProperties = () => {
     setParentContainerStyle();
 }
 
-export const setScrollAnimationProperties = (time, timingFunction) => {
+const setScrollAnimationProperties = (time, timingFunction) => {
     smoothScrollContainer.style.transition = `transform ${time} ${timingFunction}`;
 
 }
@@ -31,14 +68,10 @@ const setMainContainerStyle = () => {
 const setParentContainerStyle = () => {
     smoothScrollContainer.parentElement.style.height = containerStyles.height;
 }
-export const setScrollPosition = () => {
+const setScrollPosition = () => {
     scroll = window.scrollY || window.pageYOffset;
 }
-export const smoothScrollTo = (target, time, timingFunction) => {
-    containerStyles.transform = `translateY(${-target}px)`;
-    scrollTo(0, target);
-    setScrollAnimationProperties(time, timingFunction);
-}
+
 const moveOnScroll = () => {
     containerStyles.transform = `translateY(${-scroll}px)`;
     requestAnimationFrame(moveOnScroll);
